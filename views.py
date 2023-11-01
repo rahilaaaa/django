43bd08395465b5e86f4d93caa8e7a222dbd56f9b
rahilaaaa <mkrahila117@gmail.com>
2023-11-01 19:40:16 +0530

    add more movies in dict

--- views.py
+++ views.py
@@ -4,13 +4,24 @@ from django.shortcuts import render
 
 # Create your views here.
 def Print_Hello(request):
-    movie_details={
+    movie_details={'movies':[
+        {
         "Title":"titanic",
         "year":1998,
         "summery":"ship crash",
         "success":True
+      },
+       {
+        "Title":"Persuite of happiness",
+        "year":1999,
+        "summery":"life cahnges",
+        "success":True
+      }, {
+        "Title":"after fell",
+        "year":2009,
+        "summery":"love story",
+        "success":False
+      },
+    ]
     }
-    return render(request,'index.html',movie_details)
-        
-        
-    
+    return render(request,'index.html',movie_details) 
